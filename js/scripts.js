33344e1bfafeddfa09938bbdb8e25449cb952957
Verinikita <80643476+Verinikita@users.noreply.github.com>
2024-01-17 11:41:04 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,18 @@
 // Highlights current date on contact page
 window.addEventListener('DOMContentLoaded', event => {
     const listHoursArray = document.body.querySelectorAll('.list-hours li');
-    listHoursArray[new Date().getDay()].classList.add(('today'));
-})
+
+    // Verifica si hay elementos en listHoursArray
+    if (listHoursArray.length > 0) {
+        // Asegúrate de que el índice no exceda la longitud del array
+        const todayIndex = new Date().getDay();
+        if (todayIndex < listHoursArray.length) {
+            // Accede al elemento correspondiente y agrega la clase 'today'
+            listHoursArray[todayIndex].classList.add('today');
+        } else {
+            console.error('El índice del día actual excede la longitud del array.');
+        }
+    } else {
+        console.error('No se encontraron elementos con la clase ".list-hours li".');
+    }
+});
