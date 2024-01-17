ceb9808eb202842d9c6d2495a249a76ca50398ac
Verinikita <80643476+Verinikita@users.noreply.github.com>
2024-01-17 12:24:39 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,4 @@
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
 */
 // Highlights current date on contact page
-window.addEventListener('DOMContentLoaded', event => {
-    const listHoursArray = document.body.querySelectorAll('.list-hours li');
 
-    // Verifica si hay elementos en listHoursArray
-    if (listHoursArray.length > 0) {
-        // Asegúrate de que el índice no exceda la longitud del array
-        const todayIndex = new Date().getDay();
-        if (todayIndex < listHoursArray.length) {
-            // Accede al elemento correspondiente y agrega la clase 'today'
-            listHoursArray[todayIndex].classList.add('today');
-        } else {
-            console.error('El índice del día actual excede la longitud del array.');
-        }
-    } else {
-        console.error('No se encontraron elementos con la clase ".list-hours li".');
-    }
-});
